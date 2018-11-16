46f162c9bce8f77e077ec54787c1196e991d72b4
GRUPOGMV\jmpt <jmpt@gmv.es>
2018-11-16 17:59:39 +0100

    Both services implementation

--- server.py
+++ server.py
@@ -1,21 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 from flask import (
     Flask,
     render_template
 )
+try: 
+    from BeautifulSoup import BeautifulSoup
+except ImportError:
+    from bs4 import BeautifulSoup
+import requests
+import json
 
 # Create the application instance
 app = Flask(__name__, template_folder="templates")
 
-# Create a URL route in our application for "/"
-@app.route('/')
-def home():
-    """
-    This function just responds to the browser ULR
-    localhost:5000/
+def parseBusToObject(text):
+	busAttrs = text.split(' - ')
+	
+	bus = {
+		'name': busAttrs[0].replace('\u00a0',''),
+		'minutes': busAttrs[1].replace('\u00a0','')
+	}
+	
+	return bus
+	
+def parseHaltestelleToObject(text):
+	haltestelleAttrs = text.split(' - ')
+	
+	haltestelle = {
+		'num': haltestelleAttrs[0],
+		'name': haltestelleAttrs[1]
+	}
+	
+	return haltestelle
 
-    :return:        the rendered template 'home.html'
-    """
-    return render_template('home.html')
+def getNextBus(haltestelle, bus):
+	
+	body = {
+		'parada': haltestelle
+	}
+	
+	busUrl = "https://www.emtvalencia.es/ciudadano/modules/mod_tiempo/busca_parada.php"
+	res = []
+	request = requests.post(busUrl, data=body)
+	request.encoding = 'UTF-8'
+	response = requests.post(busUrl, data=body)
+	busList = list(response.text.split('<br/>'))
+	
+	for bus in busList:
+		parsedBus = BeautifulSoup(bus, "html.parser")
+		if parsedBus.find_all(filter=bus) != None:
+			busContents = parsedBus.find_all('span', class_="llegadaHome")
+			for content in busContents:
+				res.append(parseBusToObject(content.text))
+	
+		print(' ')
+		
+	return res
+
+def getHaltestelle(name):
+	haltestelleUrl = "https://www.emtvalencia.es/ciudadano/modules/mod_tiempo/sugiere_parada.php"
+	body = {
+		'parada': name
+	}
+	res = []
+		
+	request = requests.post(haltestelleUrl, data=body)
+	request.encoding = 'UTF-8'
+	haltestelleList = request.text
+	parsedHaltestelleList = BeautifulSoup(haltestelleList, "html.parser")
+	
+
+	for haltestelle in parsedHaltestelleList.find_all('li'):
+		res.append(parseHaltestelleToObject(haltestelle.text))
+	
+	return res
+
+# Create a URL route in our application for "/"
+@app.route('/haltestelle/<haltestelle>/', methods=['GET'])
+def haltestelle(haltestelle):
+	print(haltestelle)
+	return json.dumps(getHaltestelle(haltestelle))
+	
+@app.route('/bus/<haltestelle>/<bus>/', methods=['GET'])
+def bus(haltestelle,bus):
+	return json.dumps(getNextBus(haltestelle,bus))
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
